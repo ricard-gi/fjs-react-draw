a7464ccf44e9d54288abfaffc0efc4022485a6fa
ricard-gi <ricard.hernandez.gi@gmail.com>
2024-02-16 18:01:42 +0100

    gaue

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Pie from './Pie'
 import Relotge from './Relotge'
 import Crono from './Crono'
 import Map from './Map'
+import Gauges from './Gauges'
 
 export default () => {
 
@@ -15,7 +16,7 @@ export default () => {
         <>
             <div className="grid grid-cols-2 gap-8 ">
                 <div className="w-full h-100">
-                    <Meteo />
+                    <Gauges />
 
                 </div>
                 <div className="w-full h-100">
